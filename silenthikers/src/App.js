c7c75f8226fc4cc697c314519c7014cf9d69bed4
alisaghir1 <alisaghir543@gmail.com>
2023-10-30 14:21:24 +0200

    second step

--- silenthikers/src/App.js
+++ silenthikers/src/App.js
@@ -1,25 +1,20 @@
 import './App.css';
 import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import PhotoGallery from './pages/PhotoGallery/PhotoGallery'
+import Questions from './pages/Questions/Questions'
+import UpcomingHikes from './pages/UpcomingHikes/UpcomingHikes'
+import ContactUs from './pages/ContactUs/ContactUs'
+
 
 const App = () => {
   return (
     <Routes>
-      <Route>
-
-      </Route>
-      <Route>
-        
-      </Route>
-      <Route>
-        
-      </Route>
-      <Route>
-        
-      </Route>
-      <Route>
-        
-      </Route>
-      
+      <Route path='/' element={<Home />} ></Route>
+      <Route path='/hikes' element={<UpcomingHikes />}></Route>
+      <Route path='/images' element={<PhotoGallery />}></Route>
+      <Route path='/F&Q' element={<Questions />}></Route>
+      <Route path='/ContactUs' element={<ContactUs />}></Route >
     </Routes>
   )
 }
