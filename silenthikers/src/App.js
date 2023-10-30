dd07713511708d4c4c334a3b9caa4b211374a21e
alisaghir1 <alisaghir543@gmail.com>
2023-10-30 13:55:29 +0200

    removed the vite one and do it in create-react-app

---
+++ silenthikers/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import {Routes,Route} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route>
+
+      </Route>
+      <Route>
+        
+      </Route>
+      <Route>
+        
+      </Route>
+      <Route>
+        
+      </Route>
+      <Route>
+        
+      </Route>
+      
+    </Routes>
+  )
+}
+
+export default App
