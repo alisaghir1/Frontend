abd581ba0bca44a2bccbab1ebd8fa86878298df8
alisaghir1 <alisaghir543@gmail.com>
2023-11-08 12:01:30 +0200

    edited the footer

--- silenthikers/src/components/footer/Footer.js
+++ silenthikers/src/components/footer/Footer.js
@@ -1,30 +1,34 @@
 import './footer.css';
-import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaYoutube } from 'react-icons/fa'
+import {
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedin,
+  FaYoutube,
+} from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className='footer-main'>
-        <div className='footer-content'>
-            <div className='footer-mail-div'>
-                <span className="material-symbols-outlined">mail</span>
-                <h3 className='info'>&nbsp;info@silenthikers.com</h3>
-            </div>
-            <div className='footer-social-media'>
-                <h3>Connect with us:</h3>
-                <div className='spans'>
-                    <FaFacebook className="socialmedia-icons" size={32}/>
-                    <FaTwitter className="socialmedia-icons" size={32}/>
-                    <FaInstagram className="socialmedia-icons"  size={32}/>
-                    <FaLinkedin className='socialmedia-icons' size={32} />
-                    <FaYoutube className='socialmedia-icons' size={32} />
-                </div>
-            </div>
+    <footer className="footer-main">
+        <h3 className="info">
+          <span className="material-symbols-outlined">mail</span>
+          info@silenthikers.com
+        </h3>
+      <div className="copy-div">
+        <span>&copy;2023 All rights reserved</span>
+      </div>
+      <div className="footer-social-media">
+        <h3>Connect with us:</h3>
+        <div className="spans">
+          <FaFacebook className="socialmedia-icons" size={18} />
+          <FaTwitter className="socialmedia-icons" size={18} />
+          <FaInstagram className="socialmedia-icons" size={18} />
+          <FaLinkedin className="socialmedia-icons" size={18} />
+          <FaYoutube className="socialmedia-icons" size={18} />
         </div>
-        <div className='copy-div'>
-            <span>&copy;2023 All rights reserved</span>
-        </div>
-        </footer>
-  )
-}
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
